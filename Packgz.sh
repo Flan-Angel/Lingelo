21b6449b2f2408603c6bdede0044be9a70b66d93
Flan-Angel <flan.angel@proton.me>
2025-09-28 17:35:34 +0530

    Add files via upload

--- Packgz.sh
+++ Packgz.sh
@@ -2,6 +2,7 @@ echo "You probably want packages for your system, so like your GUI and Security 
 
 sleep 7s
 
+cd Lingelo
 cat packages.conf
 
 pacman -S --noconfirm ${ESSEN[@]}
@@ -10,10 +11,10 @@ pacman -S --noconfirm ${ESSEN[@]}
 echo "Do you want a secure system? (y/n)"
 read sec
 
-if [ $sec == y ]; then
+if [[ $sec == y ]]; then
   pacman -S --noconfirm ${SECC[@]}
 
-  if [ $bini == systemd ]; then
+  if [[ $bini == systemd ]]; then
     echo "Setting up all the yummy security... Your firewall + Apparmor"
     sleep 2s
     systemctl enable apparmor
@@ -30,12 +31,12 @@ fi
 echo "Do you want the laptop mode tools and other stuff that keeps your hardware safe? (y/n)"
 read bhardw
 
-if [ $bhardw == y ]; then
+if [[ $bhardw == y ]]; then
   echo "HardwareChan: Thanks for keeping me safe onee-san uwu"
   pacman -S --noconfirm ${HARDW[@]}
 fi
 
-if [ $bhardw == n ]; then
+if [[ $bhardw == n ]]; then
   echo "HardwareChan: Meanie :( Fuck you"
 fi
 
@@ -43,11 +44,11 @@ fi
 echo "Do you want Bluetooth and Networkmanager? (y/n)"
 read blut
 
-if [ $blut == y ]; then
+if [[ $blut == y ]]; then
   pacman -S --noconfirm ${CONN[@]}
 fi
 
-if [ $blut == n ]; then
+if [[ $blut == n ]]; then
   echo "Okay :3"
 fi
 
@@ -55,10 +56,10 @@ fi
 echo "Do you want to have sound? (y/n)"
 read bsnd
 
-if [ $bsnd == y]; then
+if [[ $bsnd == y ]]; then
   pacman -S --noconfirm ${SOUND[@]}
 fi
-if [ $bsnd == n]; then
+if [[ $bsnd == n ]]; then
   echo ">_<"
 fi
 
@@ -66,11 +67,11 @@ fi
 echo "Do you want text related stuff? Like text editors and e-book viewers"
 read btxt
 
-if [ btxt == y ]; then
+if [[ btxt == y ]]; then
   pacman -S --noconfirm ${TEXT[@]}
 fi
 
-if [ btxt == n ]; then
+if [[ btxt == n ]]; then
   echo "Mmmmhmm."
 fi
 
