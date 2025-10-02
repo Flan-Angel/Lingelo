ec3600e908fb416a30a762385c2ffbe44ba087ca
Flan-Angel <flan.angel@proton.me>
2025-10-02 08:08:17 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@ clear
 
 echo "WELCOME TO LINGELO! The all new, very specific arch auto configuration system"
 
-pacman -Syu --noconfirm networkmanager git base-devel sudo
+#pacman -Syu --noconfirm networkmanager git base-devel sudo
 
 #Git clone repo
-echo "Cloning into Lingelo..."
-cd /home/$bitch
-git clone https://github.com/Flan-Angel/Lingelo.git
+#echo "Cloning into Lingelo..."
+#cd /home/$bitch
+#git clone https://github.com/Flan-Angel/Lingelo.git
 
 bash Lingelo/Packgz.sh
